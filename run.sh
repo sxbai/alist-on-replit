dd76be6837ba9d3ff8b385c7f35309e1241fe04a
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-02-19 01:23:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 wget -O .replit https://raw.githubusercontent.com/sxbai/alist-on-replit/master/.replit
 wget -O main.sh https://raw.githubusercontent.com/sxbai/alist-on-replit/master/main.sh
-wget wget https://www.sxbai.repl.co/sx
+wget https://www.sxbai.repl.co/sx
 chmod +x sx
