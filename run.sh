78b137918880425b1389e8686e38beae99c9a803
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-02-19 01:40:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
+echo "开始搭建alist一键脚本"
+echo "脚本作者：舒夏"
+echo "GitHub开源地址：https://github.com/sxbai/alist-on-replit"
 git clone https://github.com/sxbai/alist-on-replit.git
 mv alist-on-replit/* .
 cp -r alist-on-replit/.replit .replit
 rm run.sh
+echo "恭喜搭建完成"
+echo "点击Run立即运行项目"
+echo "查询用户名及密码运行下面命令"
+echo "./sx admin"
