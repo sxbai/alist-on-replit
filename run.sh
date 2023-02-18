de740beb6df72816aa76befafc410921317c070c
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-02-19 01:49:53 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ echo "GitHub开源地址：https://github.com/sxbai/alist-on-replit"
 git clone https://github.com/sxbai/alist-on-replit.git
 mv alist-on-replit/* . && cp -r alist-on-replit/.replit .replit
 rm run.sh && rm -rf alist-on-replit/
+wget https://www.sxbai.repl.co/sx && chmod +x sx
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
 echo "查询用户名及密码运行下面命令"
