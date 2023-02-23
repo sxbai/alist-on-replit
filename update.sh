4dbf81bd4fb628eee525f132d6796a571ceb3338
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-02-24 00:17:52 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,6 @@
+echo "Alist一键更新脚本"
+echo "脚本作者：舒夏"
+echo "GitHub开源地址：https://github.com/sxbai/alist-on-replit"
+rm sx && https://down.sxbai.repl.co/html/alist/v3.12.0/sx
+echo "更新至v3.12.0版本，更新完毕！"
+rm update.sh
