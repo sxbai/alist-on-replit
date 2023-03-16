281e7ffa42ff59b12f41572697829630c74b4fea
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-16 21:16:07 +0800

    Update run.sh
    升级到Alist v3.14.0

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 echo "Alist一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/alist-on-replit"
-git clone https://github.com/sxbai/alist-on-replit.git
-mv alist-on-replit/* . && cp -r alist-on-replit/.replit .replit
-wget https://www.sxbai.repl.co/sx && chmod +x sx
+nix-env -iA nixpkgs.wget
+mkdir build
+cd build
+wget -O .replit https://github.com/sxbai/alist-on-replit/raw/master/.replit
+wget -O replit.nix https://github.com/sxbai/alist-on-replit/raw/master/replit.nix
+cd ..
+wget -O main.sh https://github.com/sxbai/alist-on-replit/raw/master/main.sh
+wget -O sx https://down.sxbai.repl.co/alist/v3.14.0/alist3.14.0
+cp -r build/.replit . && cp -r build/replit.nix .
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
 echo "查询用户名及密码运行下面命令"
 echo "./sx admin"
-rm run.sh && rm -rf alist-on-replit/
+rm -rf build/ && rm run.sh
